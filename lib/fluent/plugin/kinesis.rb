c573c7d3dadcac1c8420ac4affe1e1a4ec2787aa
wanfeng.hu <wanfeng.hu@ihandysoft.com>
2019-07-17 10:31:16 +0800

    Modified kinesis aggregation method

--- lib/fluent/plugin/kinesis.rb
+++ lib/fluent/plugin/kinesis.rb
@@ -71,6 +71,7 @@ module Fluent
       end
 
       config_param :debug, :bool, default: false
+      config_param :max_records_per_call, :integer, default: 128
 
       helpers :formatter, :inject
 
@@ -141,6 +142,29 @@ module Fluent
             log.debug(sprintf "Write chunk %s / %3d records / %4d KB", unique_id, batch.size, size/1024)
             batch_request_with_retry(batch, &block)
             log.debug("Finish writing chunk")
+          end
+        end
+      end
+
+      def write_records_batch2(chunk, &block)
+        unique_id = chunk.dump_unique_id_hex(chunk.unique_id)
+        chunk.open do |io|
+          records = msgpack_unpacker(io).to_enum
+          batches = []
+          sizes = []
+          split_to_batches(records) do |batch, size|
+            batches << batch
+            sizes << size            
+          end
+          (0..batches.size).step(@max_records_per_call) do |i|
+            batches_slice = batches[i..i+@max_records_per_call-1]
+            size = sizes[i..i+@max_records_per_call-1].inject(:+)
+            batches_size = batches_slice.map(&:size).inject(:+)
+            if not size.nil?
+              log.debug(sprintf "Write chunk %s / %3d batches / %3d records / %4d KB", unique_id, batches_slice.size, batches_size, size/1024)
+              batch_request_with_retry(batches_slice, &block)
+              log.debug("Finish writing chunk")
+            end
           end
         end
       end
--- lib/fluent/plugin/out_kinesis_streams_aggregated.rb
+++ lib/fluent/plugin/out_kinesis_streams_aggregated.rb
@@ -23,7 +23,7 @@ module Fluent
 
       RequestType = :streams_aggregated
       BatchRequestLimitCount = 100_000
-      BatchRequestLimitSize  = 1024 * 1024
+      BatchRequestLimitSize  = 1024 * 1024 # 128*1024
       include KinesisHelper::API::BatchRequest
 
       config_param :stream_name, :string
@@ -42,16 +42,35 @@ module Fluent
         end
       end
 
+      # def write(chunk)
+      #   write_records_batch(chunk) do |batch|
+      #     key = @partition_key_generator.call
+      #     records = batch.map{|(data)|data}
+      #     client.put_records(
+      #       stream_name: @stream_name,
+      #       records: [{
+      #         partition_key: key,
+      #         data: aggregator.aggregate(records, key),
+      #       }],
+      #     )
+      #   end
+      # end
+
       def write(chunk)
-        write_records_batch(chunk) do |batch|
-          key = @partition_key_generator.call
-          records = batch.map{|(data)|data}
+        write_records_batch2(chunk) do |batches|
+          records_to_send = []
+          batches.each do |batch|
+            key = @partition_key_generator.call
+            records = batch.map{|(data)|data}
+            regularized_agg_record = {
+              partition_key: key,
+              data: aggregator.aggregate(records, key)
+            }
+            records_to_send << regularized_agg_record
+          end
           client.put_records(
             stream_name: @stream_name,
-            records: [{
-              partition_key: key,
-              data: aggregator.aggregate(records, key),
-            }],
+            records: records_to_send
           )
         end
       end
